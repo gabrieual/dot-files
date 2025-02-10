b794d80d6658369c84611c7638cc44b2673ad409
Gabriel M <gabrielalmeidamaciel194@gmail.com>
2025-02-10 20:32:21 -0300

    screenshot and more

---
+++ hypr/scripts/print.sh
@@ -0,0 +1,11 @@
+DIR="$HOME/Imagens/Screenshots"
+FILENAME="screenshot_$(date '+%Y-%m-%d_%H-%M-%S').png"
+FILEPATH="$DIR/$FILENAME"
+
+mkdir -p "$DIR"
+
+grim -g "$(slurp)" "$FILEPATH"
+wl-copy < "$FILEPATH"
+
+# notify-send "Screenshot tirado!" "Salvo em $FILEPATH e copiado para a área de transferência." --icon=dialog-information
+
